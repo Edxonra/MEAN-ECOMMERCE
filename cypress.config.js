4209a5a5d5d8f1a464d38b5522e0e100687d46b5
Edxon <edxonrodrigueza@gmail.com>
2022-12-19 23:59:39 -0600

    Updating packages

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
 const { defineConfig } = require("cypress");
-const seed = require('./Backend/seeder.js')
+const seed = require("./Backend/seeder.js");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on,config){
-      on('task',{
-        async "seed"(){
-          await seed()
-          return null
-        }
-      })
-    }
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      on("task", {
+        async seed() {
+          await seed();
+          return null;
+        },
+      });
+    },
   }
 });
